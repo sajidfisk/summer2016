5f3b8cab572258224980c8121c5f3c746f8b63fe
Sajid Hussain <sajid.fisk@gmail.com>
2016-06-02 13:25:30 -0500

    angular js example 1

---
+++ angularjs/myApp.js
@@ -0,0 +1,1 @@
+ar app = angular.module('myApp', []);
---
+++ angularjs/myScript.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.firstName= "Jane";
+    $scope.lastName= "Doe";
+    $scope.myColor = "lightblue";
+    $scope.established = 1866;
+    $scope.year = 2016;
+    $scope.person = {firstName:'Jazmyn', lastName:'Scott'};
+    $scope.students = [
+    	{firstName: "Denise", lastName:'Buliga'},
+    	{firstName: "Erik", lastName:'Shaw'},
+    	{firstName: "Maya", lastName:'Walters'},
+    	{firstName: "Jada", lastName:'Prendergast'},
+    	{firstName: "Jazmyn", lastName:'Scott'},
+    ];
+});
