17fbc9e152ebfce544a5c14e7e0806905dae030c
Sajid Hussain <sajid.fisk@gmail.com>
2016-06-03 08:49:54 -0500

    moving directive to model

--- angularjs/ex1Ctrl.js
+++ angularjs/ex1Ctrl.js
@@ -30,13 +30,3 @@ app.controller('myCtrl', function($scope) {
     $scope.myAddress="sajid.fisk@gmail.com";
     $scope.fiskEmail = "shussain@fisk.edu"
 });
-
-
-app.directive("summer2016", function(){
-    return{
-        template:   "<a href='http://www.fisk.edu'>Fisk University</a> | " +
-                    "<a href='http://github.com/sajidfisk/summer2016'>GitHub - Sajid</a> | " +
-                    "<a href='http://w3schools.com'>w3schools</a>"
-    };
-
-});
--- angularjs/ex1Model.js
+++ angularjs/ex1Model.js
@@ -1,1 +1,10 @@
 app = angular.module('myApp', []);
+
+app.directive("summer2016", function(){
+    return{
+        template:   "<a href='http://www.fisk.edu'>Fisk University</a> | " +
+                    "<a href='http://github.com/sajidfisk/summer2016'>GitHub - Sajid</a> | " +
+                    "<a href='http://w3schools.com'>w3schools</a>"
+    };
+
+});
